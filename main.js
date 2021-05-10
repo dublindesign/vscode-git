b92795c482f7fd68f25791efb39cf2a35c592985
dublindesign <mint@codeacade.ie>
2021-05-10 15:38:06 +0100

    soso Monday

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 
-function logA() {
-console.log('a')
-}
+Aa = new Promise(r=> {
+    setTimeout(()=>{
+        console.log("Aaaa")
+    }, 2000)
+    r("aa")
+}).then(console.log)
 
-function logB() {
-    console.log('b')
-    }
+
+Bb = new Promise(r=> {
+    setTimeout(()=>{
+        console.log("Bbbb")
+    }, 500)
+    r("bb")
+})
+
+
+Cc = new Promise(r=> {
+    setTimeout(()=>{
+        console.log("Cccccc")
+    }, 2500)
+    r("cc")
+})
+
+
+Dd = new Promise(r=> {
+    setTimeout(()=>{
+        console.log("Ddddddd")
+    }, 1500)
+    r("dd")
+})
+
+
+
+console.log("Ddddddd")
+console.log("Ddddddd")
